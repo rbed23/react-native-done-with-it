b2d29c8b869caf3244ab0c0c8f105d0a2dd05e04
Ryan B <rbed23@gmail.com>
2020-09-08 22:38:32 +0300

    stack navigator

--- App.js
+++ App.js
@@ -1,32 +1,35 @@
 import React, {useState, useEffect} from 'react';
-import { StyleSheet, View, StatusBar, Switch, Button, Image } from 'react-native';
-import { MaterialCommunityIcons } from '@expo/vector-icons';
+import { StyleSheet, View, Text } from 'react-native';
 
-import * as ImagePicker from 'expo-image-picker';
-
-import Home from './app/screens/home';
-import Items from './app/screens/items';
-import AppButton from './app/components/AppButton';
-import Card from './app/components/Card';
-import ItemDetails from './app/screens/itemDetails';
-import Messages from './app/screens/messages';
-import Account from './app/screens/myAccount';
-import Icon from './app/components/Icon';
-import colors from './app/globals/colors';
-import Selection from './app/components/Selection';
-import Listings from './app/screens/listings';
+import { createStackNavigator } from '@react-navigation/stack';
+import { NavigationContainer} from '@react-navigation/native';
 import Screen from './app/components/Screen'
-import AppInput from './app/components/AppInput';
-import AppPicker from './app/components/AppPicker';
-import Login from './app/screens/login';
-import Register from './app/screens/register';
-import ListingEdit from './app/screens/listingEdit';
-import ListItem from './app/components/ListItem';
-import AppImageInput from './app/components/AppImageInput';
-import AppImageInputList from './app/components/AppImageInputList';
+
+const Tweets = () => (
+  <Screen>
+    <Text>Tweets</Text>
+  </Screen>
+)
+
+const TweetDetails = () => (
+  <Screen>
+    <Text>Tweet Details</Text>
+  </Screen>
+)
 
 
+const Stack = createStackNavigator();
+const StackNavigator = () => (
+  <Stack.Navigator initialRouteName="Tweets">
+    <Stack.Screen name='Tweets' component={Tweets} />
+    <Stack.Screen name='TweetDetails' component={TweetDetails} />
+  </Stack.Navigator>
+)
 export default function App() {
 
-  return <ListingEdit />;
+  return (
+    <NavigationContainer>
+      <StackNavigator />
+    </NavigationContainer>
+  );
 }
