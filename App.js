acf7be23b30e8eda9a863e809fa9c06ed7ed74b7
Ryan B <rbed23@gmail.com>
2020-09-15 19:16:42 +0300

    added login functionality using AuthContext

--- App.js
+++ App.js
@@ -1,18 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import { NavigationContainer } from '@react-navigation/native';
 import navigationTheme from './app/navigation/navigationTheme';
 import AppNavigator from './app/navigation/appNavigator';
+import AuthNavigator from './app/navigation/authNavigator';
 import OfflineNotice from './app/components/OfflineNotice';
+import AuthContext from './app/auth/context';
 
 export default function App() {
 
+  const [user, setUser] = useState();
+
   return (
-    <>
+    <AuthContext.Provider value={{ user, setUser }}>
       <OfflineNotice />
       <NavigationContainer theme={navigationTheme}>
-        <AppNavigator />
+        { user ? <AppNavigator /> : <AuthNavigator /> }
       </NavigationContainer>
-    </>
+
+    </AuthContext.Provider>
   );
 }
--- app/screens/login.js
+++ app/screens/login.js
@@ -1,11 +1,13 @@
-import React from 'react'
+import React, { useState, useContext } from 'react'
 import { StyleSheet, Image } from 'react-native'
 
 import * as Yup from 'yup';
+import jwtDecode from 'jwt-decode';
 
 import Screen from '../components/Screen'
-import { AppForm, AppFormField, AppFormSubmitButton } from '../components/forms';
-
+import { AppFormErrorMssg, AppForm, AppFormField, AppFormSubmitButton } from '../components/forms';
+import authApi from '../api/auth';
+import AuthContext from '../auth/context';
 
 const validationSchema = Yup.object().shape({
     email: Yup.string().required().email().label('Email'),
@@ -15,18 +17,39 @@ const validationSchema = Yup.object().shape({
 
 export default function Login(props) {
 
+    const authContext = useContext(AuthContext)
+    
+    const [loginError, setLoginError] = useState(false);
+    const [errorMssg, setErrorMssg] = useState();
+    
+    const handleSubmit = async (loginInfo) => {
+        const result = await authApi.login(loginInfo.email, loginInfo.password)
+        if (!result.ok) {
+            setLoginError(true)
+            setErrorMssg(result.data.error)
+            return null
+        }
+
+        setLoginError(false);
+        const user = jwtDecode(result.data);
+        authContext.setUser(user)
+    }
+
     return (
         <Screen style={styles.container}>
             <Image 
                 style={styles.logo}
                 source={require('../assets/logo-red.png')}
-                />
+            />
             
             <AppForm
                 initialValues={{email: '', password: ''}}
-                onSubmit={vals=>console.log(vals)}
+                onSubmit={handleSubmit}
                 validationSchema={validationSchema}
             >
+
+                <AppFormErrorMssg error={errorMssg} visible={loginError} />
+
                 <AppFormField 
                     autoCapitalize='none'
                     autoCorrect={false}
